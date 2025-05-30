a4cc7a07095a9b90d27b7260ebefebd6c470afa6
Kinjal Chauhan <kinjal.chauhan@builder.ai>
2025-05-30 22:50:49 +0530

    added basic checks, empty string, two numbers, many numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,6 @@
 class StringCalculator
   def add(numbers)
+    return 0 if numbers.strip == ""
     numbers.split(",").map(&:to_i).sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,9 +1,23 @@
 require "string_calculator"
 
 RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
   describe '#add' do
-    it 'returns the sum of its arguments' do
-      expect(StringCalculator.new.add("1,2")).to eq(3)
+    it "returns 0 for empty string" do
+      expect(calculator.add("")).to eq(0)
+    end
+
+    it "returns 1 for single number string" do
+      expect(calculator.add("1 ")).to eq(1)
+    end
+
+    it 'returns the sum of two numbers' do
+      expect(calculator.add("1,2")).to eq(3)
+    end
+
+    it 'returns the sum of any numbers' do
+      expect(calculator.add("1,2,6,4,3,5,8,43,3")).to eq(75)
     end
   end
 end
