d6f01929873f1a7c4e9d86a0e6b68d090c3aaa9e
Kinjal Chauhan <kinjal.chauhan@builder.ai>
2025-05-30 23:23:28 +0530

    added checks for different delimiter, negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,25 @@
 class StringCalculator
-  def add(numbers)
-    return 0 if numbers.strip == ""
-    numbers.split(",").map(&:to_i).sum
+  def add(input)
+    return 0 if input.strip.empty?
+
+    separator = ","
+    if input.start_with?("//")
+      parts = input.split("\n", 2)
+      separator = parts[0][2..]
+      input = parts[1]
+    end
+
+    input = input.gsub("\n", separator)
+    numbers = split_string(input, separator).map(&:to_i)
+    negatives = numbers.select(&:negative?)
+    raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+
+    numbers.sum
+  end
+
+  private
+
+  def split_string(input, separator)
+    input.split(separator)
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,20 +4,36 @@ RSpec.describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
   describe '#add' do
-    it "returns 0 for empty string" do
+    it "should return 0 for empty string" do
       expect(calculator.add("")).to eq(0)
     end
 
-    it "returns 1 for single number string" do
+    it "should return 1 for single number string" do
       expect(calculator.add("1 ")).to eq(1)
     end
 
-    it 'returns the sum of two numbers' do
+    it 'should return the sum of two numbers' do
       expect(calculator.add("1,2")).to eq(3)
     end
 
-    it 'returns the sum of any numbers' do
+    it 'should return the sum of any numbers' do
       expect(calculator.add("1,2,6,4,3,5,8,43,3")).to eq(75)
+    end
+
+    it 'should handle new lines between numbers' do
+      expect(calculator.add("1,2\n3")).to eq(6)
+    end
+
+    it "sholud support custom delimiter" do
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end
+
+    it "raises error for single negative number" do
+      expect { calculator.add("1,-2,13") }.to raise_error("negative numbers not allowed: -2")
+    end
+
+    it "raises error for multiple negative numbers" do
+      expect { calculator.add("1,-2,-4,13") }.to raise_error("negative numbers not allowed: -2, -4")
     end
   end
 end
